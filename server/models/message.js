1b3befe0a1930243fdd5badf4d2218dca7707e73
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-01 15:19:03 +0530

    create message model

---
+++ server/models/message.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    text:{
+        type:String,
+        required:true
+    },
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+        
+    },
+    receiverId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+        
+    }
+   
+    
+},{timestamps:true})
+
+const Message = mongoose.model('Message',messageSchema)
+module.exports = Message
