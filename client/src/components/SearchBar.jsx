8f69a2fcdf1fb203d065fe985276c829830cd16a
ankitjhagithub21 <ajha45075@gmail.com>
2024-08-04 11:15:22 +0530

    added searching functionality

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { GoSearch } from "react-icons/go"
-const SearchBar = () => {
-    const [query,setQuery] = useState('')
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-    }
-  return (
-    <form className='border rounded-lg flex items-center  p-2 h-[10%] gap-2' onSubmit={handleSubmit}>
-      <input type="text" value={query} onChange={(e)=>setQuery(e.target.value)} className='w-full pl-2'/>
-      <GoSearch size={25}/>
-    </form>
-  )
+
+const SearchBar = ({ query, setQuery }) => {
+   
+
+    return (
+        <div className='border rounded-lg flex items-center p-2 h-[10%] gap-2'>
+            <input
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className='w-full pl-2'
+                placeholder='Search users...'
+            />
+            <GoSearch size={25} />
+        </div>
+    )
 }
 
 export default SearchBar
--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -3,10 +3,10 @@ import User from './User'
 import SearchBar from './SearchBar'
 import Profile from './Profile'
 
-
 const Users = () => {
     const [users, setUsers] = useState([])
-   
+    const [query, setQuery] = useState('')
+
     const fetchUsers = async () => {
         try {
             const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/users`, {
@@ -26,22 +26,24 @@ const Users = () => {
         fetchUsers()
     }, [])
 
+    const filteredUsers = users.filter(user =>
+        user.name.toLowerCase().includes(query.toLowerCase())
+    )
+
     return (
-        <div className={`md:w-1/4 w-full   flex flex-col gap-2 border-r p-2`}>
-         <SearchBar/>
+        <div className={`md:w-1/4 w-full flex flex-col gap-2 border-r p-2`}>
+            <SearchBar query={query} setQuery={setQuery} />
 
-          <div className='flex flex-col gap-3 h-[80%] scroll p-2 overflow-y-scroll overflow-x-hidden'>
-          {
-                users.map((user) => {
-                    return <User key={user._id} user={user}/>
-                    
-                })
-            }
-           
-          
-          </div>
-          <Profile/>
-
+            <div className='flex flex-col gap-3 h-[80%] scroll p-2 overflow-y-scroll overflow-x-hidden'>
+                {filteredUsers.length === 0 ? (
+                    <p className="text-center text-lg">No users found</p>
+                ) : (
+                    filteredUsers.map((user) => {
+                        return <User key={user._id} user={user} />
+                    })
+                )}
+            </div>
+            <Profile />
         </div>
     )
 }
